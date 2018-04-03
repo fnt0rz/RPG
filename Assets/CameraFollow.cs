a413a611598fe6eac64a3ffe9cc4f30ec8ee5e8c
Yoeri van de Noort <faint0rz@gmail.com>
2018-04-03 22:27:19 +0200

    added raycasting

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-private void LateUpdate() {
-	var player = GameObject.FindGameObjectWithTag("Player");
-	transform.position = player.transform.position;
-}
+	GameObject player;	
+
+	private void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	private void LateUpdate() {
+		transform.position = player.transform.position;
+	}
 
 }
