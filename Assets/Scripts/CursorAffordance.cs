4e7bd484597b671b5a098b86eecea655a49e6098
Yoeri van de Noort <faint0rz@gmail.com>
2018-04-06 00:45:14 +0200

    Mouse icons

---
+++ Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor,attackCursor,unknownCursor = null;
+	[SerializeField] Vector2 cursorHotSpot = new Vector2(96f,96f);
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (cameraRaycaster.layerHit)
+		{
+			case Layer.Walkable:
+				Cursor.SetCursor(walkCursor,cursorHotSpot,CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor(attackCursor,cursorHotSpot,CursorMode.Auto);
+				break;
+			default:
+				Cursor.SetCursor(unknownCursor,cursorHotSpot,CursorMode.Auto);
+				return;
+		}
+		
+	}
+}
